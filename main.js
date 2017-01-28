9ff057f66a37c75f1880d0ae0f9946d3ef310b0d
Ben Allen <me@benallen.info>
2017-01-28 23:55:20 +0000

    Unbinding mechanism

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ var id = process.argv[2] || 0;
 var name = process.argv[3] || 'Anonymous Name';
 
 var options = {
-  name: name,    // set device name when advertising (Linux only)
-  txPowerLevel: -100, // override TX Power Level, default value is -21,
+  name: 'Beacon',    // set device name when advertising (Linux only)
+  txPowerLevel: -22, // override TX Power Level, default value is -21,
   tlmCount: 2,       // 2 TLM frames
   tlmPeriod: 10      // every 10 advertisements
 };
@@ -16,3 +16,7 @@ var url = 'http://' + id;
 console.log('Advertising on url: ' + url + ' with name: ' + name)
 
 eddystoneBeacon.advertiseUrl(url, [options]);
+
+// var url = 'http://example.com';
+//
+// eddystoneBeacon.advertiseUrl(url, [options]);
